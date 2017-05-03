37aeb2b92cca99a34824827c9c68116b0f7a9655
Cesar Rodas <cesar@rodas.me>
2017-05-03 14:19:45 -0400

    Add a better solution for newer phpunit (on php 7.0)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,4 +28,8 @@ if( false !== getenv( 'WP_TESTS_DIR' ) ) {
 	require dirname( dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) ) . '/tests/phpunit/includes/bootstrap.php';
 }
 
+if (!class_exists('\PHPUnit_Framework_TestCase')) {
+    require __DIR__ . '/phpunit-compat.php';
+}
+
 include_once('ActionScheduler_UnitTestCase.php');

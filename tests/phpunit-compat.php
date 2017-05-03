9256b9ac4bd411bf0245efbdcf3f5500b4d940cf
Cesar Rodas <cesar@rodas.me>
2017-05-03 15:17:25 -0400

    Improve phpunit-compat

--- tests/phpunit-compat.php
+++ tests/phpunit-compat.php
@@ -1,8 +1,363 @@
 <?php
 
-class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase
-{
+class PHPUnit_Framework_Assert extends PHPUnit\Framework\Assert {
 }
 
 class PHPUnit_Framework_Exception extends PHPUnit\Framework\Exception {
 }
+
+class PHPUnit_Framework_AssertionFailedError extends PHPUnit\Framework\AssertionFailedError {
+}
+
+class PHPUnit_Framework_BaseTestListener extends PHPUnit\Framework\BaseTestListener {
+}
+
+class PHPUnit_Framework_CodeCoverageException extends PHPUnit\Framework\CodeCoverageException {
+}
+
+class PHPUnit_Framework_Constraint_Constraint extends PHPUnit\Framework\Constraint\Constraint {
+}
+
+class PHPUnit_Framework_Constraint_ArrayHasKey extends PHPUnit\Framework\Constraint\ArrayHasKey {
+}
+
+class PHPUnit_Framework_Constraint_ArraySubset extends PHPUnit\Framework\Constraint\ArraySubset {
+}
+
+class PHPUnit_Framework_Constraint_Composite extends PHPUnit\Framework\Constraint\Composite {
+}
+
+class PHPUnit_Framework_Constraint_Attribute extends PHPUnit\Framework\Constraint\Attribute {
+}
+
+class PHPUnit_Framework_Constraint_Callback extends PHPUnit\Framework\Constraint\Callback {
+}
+
+class PHPUnit_Framework_Constraint_ClassHasAttribute extends PHPUnit\Framework\Constraint\ClassHasAttribute {
+}
+
+class PHPUnit_Framework_Constraint_ClassHasStaticAttribute extends PHPUnit\Framework\Constraint\ClassHasStaticAttribute {
+}
+
+class PHPUnit_Framework_Constraint_Count extends PHPUnit\Framework\Constraint\Count {
+}
+
+class PHPUnit_Framework_Constraint_DirectoryExists extends PHPUnit\Framework\Constraint\DirectoryExists {
+}
+
+class PHPUnit_Framework_Constraint_Exception extends PHPUnit\Framework\Constraint\Exception {
+}
+
+class PHPUnit_Framework_Constraint_ExceptionCode extends PHPUnit\Framework\Constraint\ExceptionCode {
+}
+
+class PHPUnit_Framework_Constraint_ExceptionMessage extends PHPUnit\Framework\Constraint\ExceptionMessage {
+}
+
+class PHPUnit_Framework_Constraint_ExceptionMessageRegularExpression extends PHPUnit\Framework\Constraint\ExceptionMessageRegularExpression {
+}
+
+class PHPUnit_Framework_Constraint_FileExists extends PHPUnit\Framework\Constraint\FileExists {
+}
+
+class PHPUnit_Framework_Constraint_GreaterThan extends PHPUnit\Framework\Constraint\GreaterThan {
+}
+
+class PHPUnit_Framework_Constraint_IsAnything extends PHPUnit\Framework\Constraint\IsAnything {
+}
+
+class PHPUnit_Framework_Constraint_IsEmpty extends PHPUnit\Framework\Constraint\IsEmpty {
+}
+
+class PHPUnit_Framework_Constraint_IsEqual extends PHPUnit\Framework\Constraint\IsEqual {
+}
+
+class PHPUnit_Framework_Constraint_IsFalse extends PHPUnit\Framework\Constraint\IsFalse {
+}
+
+class PHPUnit_Framework_Constraint_IsFinite extends PHPUnit\Framework\Constraint\IsFinite {
+}
+
+class PHPUnit_Framework_Constraint_IsIdentical extends PHPUnit\Framework\Constraint\IsIdentical {
+}
+
+class PHPUnit_Framework_Constraint_IsInfinite extends PHPUnit\Framework\Constraint\IsInfinite {
+}
+
+class PHPUnit_Framework_Constraint_IsInstanceOf extends PHPUnit\Framework\Constraint\IsInstanceOf {
+}
+
+class PHPUnit_Framework_Constraint_IsJson extends PHPUnit\Framework\Constraint\IsJson {
+}
+
+class PHPUnit_Framework_Constraint_IsNan extends PHPUnit\Framework\Constraint\IsNan {
+}
+
+class PHPUnit_Framework_Constraint_IsNull extends PHPUnit\Framework\Constraint\IsNull {
+}
+
+class PHPUnit_Framework_Constraint_IsReadable extends PHPUnit\Framework\Constraint\IsReadable {
+}
+
+class PHPUnit_Framework_Constraint_IsTrue extends PHPUnit\Framework\Constraint\IsTrue {
+}
+
+class PHPUnit_Framework_Constraint_IsType extends PHPUnit\Framework\Constraint\IsType {
+}
+
+class PHPUnit_Framework_Constraint_IsWritable extends PHPUnit\Framework\Constraint\IsWritable {
+}
+
+class PHPUnit_Framework_Constraint_JsonMatches extends PHPUnit\Framework\Constraint\JsonMatches {
+}
+
+class PHPUnit_Framework_Constraint_JsonMatchesErrorMessageProvider extends PHPUnit\Framework\Constraint\JsonMatchesErrorMessageProvider {
+}
+
+class PHPUnit_Framework_Constraint_LessThan extends PHPUnit\Framework\Constraint\LessThan {
+}
+
+class PHPUnit_Framework_Constraint_LogicalAnd extends PHPUnit\Framework\Constraint\LogicalAnd {
+}
+
+class PHPUnit_Framework_Constraint_LogicalNot extends PHPUnit\Framework\Constraint\LogicalNot {
+}
+
+class PHPUnit_Framework_Constraint_LogicalOr extends PHPUnit\Framework\Constraint\LogicalOr {
+}
+
+class PHPUnit_Framework_Constraint_LogicalXor extends PHPUnit\Framework\Constraint\LogicalXor {
+}
+
+class PHPUnit_Framework_Constraint_ObjectHasAttribute extends PHPUnit\Framework\Constraint\ObjectHasAttribute {
+}
+
+class PHPUnit_Framework_Constraint_RegularExpression extends PHPUnit\Framework\Constraint\RegularExpression {
+}
+
+class PHPUnit_Framework_Constraint_SameSize extends PHPUnit\Framework\Constraint\SameSize {
+}
+
+class PHPUnit_Framework_Constraint_StringContains extends PHPUnit\Framework\Constraint\StringContains {
+}
+
+class PHPUnit_Framework_Constraint_StringEndsWith extends PHPUnit\Framework\Constraint\StringEndsWith {
+}
+
+class PHPUnit_Framework_Constraint_StringMatchesFormatDescription extends PHPUnit\Framework\Constraint\StringMatchesFormatDescription {
+}
+
+class PHPUnit_Framework_Constraint_StringStartsWith extends PHPUnit\Framework\Constraint\StringStartsWith {
+}
+
+class PHPUnit_Framework_Constraint_TraversableContains extends PHPUnit\Framework\Constraint\TraversableContains {
+}
+
+class PHPUnit_Framework_Constraint_TraversableContainsOnly extends PHPUnit\Framework\Constraint\TraversableContainsOnly {
+}
+
+class PHPUnit_Framework_RiskyTestError extends PHPUnit\Framework\RiskyTestError {
+}
+
+class PHPUnit_Framework_CoveredCodeNotExecutedException extends PHPUnit\Framework\CoveredCodeNotExecutedException {
+}
+
+class PHPUnit_Framework_TestSuite extends PHPUnit\Framework\TestSuite {
+}
+
+class PHPUnit_Framework_DataProviderTestSuite extends PHPUnit\Framework\DataProviderTestSuite {
+}
+
+class PHPUnit_Framework_Error_Error extends PHPUnit\Framework\Error\Error {
+}
+
+class PHPUnit_Framework_Error_Deprecated extends PHPUnit\Framework\Error\Deprecated {
+}
+
+class PHPUnit_Framework_Error_Notice extends PHPUnit\Framework\Error\Notice {
+}
+
+class PHPUnit_Framework_Error_Warning extends PHPUnit\Framework\Error\Warning {
+}
+
+class PHPUnit_Framework_ExceptionWrapper extends PHPUnit\Framework\ExceptionWrapper {
+}
+
+class PHPUnit_Framework_ExpectationFailedException extends PHPUnit\Framework\ExpectationFailedException {
+}
+
+class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase {
+}
+
+class PHPUnit_Framework_IncompleteTestCase extends PHPUnit\Framework\IncompleteTestCase {
+}
+
+class PHPUnit_Framework_IncompleteTestError extends PHPUnit\Framework\IncompleteTestError {
+}
+
+class PHPUnit_Framework_InvalidCoversTargetException extends PHPUnit\Framework\InvalidCoversTargetException {
+}
+
+class PHPUnit_Framework_MissingCoversAnnotationException extends PHPUnit\Framework\MissingCoversAnnotationException {
+}
+
+class PHPUnit_Framework_OutputError extends PHPUnit\Framework\OutputError {
+}
+
+class PHPUnit_Framework_SkippedTestCase extends PHPUnit\Framework\SkippedTestCase {
+}
+
+class PHPUnit_Framework_SkippedTestError extends PHPUnit\Framework\SkippedTestError {
+}
+
+class PHPUnit_Framework_SkippedTestSuiteError extends PHPUnit\Framework\SkippedTestSuiteError {
+}
+
+class PHPUnit_Framework_SyntheticError extends PHPUnit\Framework\SyntheticError {
+}
+
+class PHPUnit_Framework_TestFailure extends PHPUnit\Framework\TestFailure {
+}
+
+class PHPUnit_Framework_TestResult extends PHPUnit\Framework\TestResult {
+}
+
+class PHPUnit_Framework_TestSuiteIterator extends PHPUnit\Framework\TestSuiteIterator {
+}
+
+class PHPUnit_Framework_UnintentionallyCoveredCodeError extends PHPUnit\Framework\UnintentionallyCoveredCodeError {
+}
+
+class PHPUnit_Framework_Warning extends PHPUnit\Framework\Warning {
+}
+
+class PHPUnit_Framework_WarningTestCase extends PHPUnit\Framework\WarningTestCase {
+}
+
+class PHPUnit_Runner_BaseTestRunner extends PHPUnit\Runner\BaseTestRunner {
+}
+
+class PHPUnit_Runner_Exception extends PHPUnit\Runner\Exception {
+}
+
+class PHPUnit_Runner_Filter_GroupFilterIterator extends PHPUnit\Runner\Filter\GroupFilterIterator {
+}
+
+class PHPUnit_Runner_Filter_ExcludeGroupFilterIterator extends PHPUnit\Runner\Filter\ExcludeGroupFilterIterator {
+}
+
+class PHPUnit_Runner_Filter_Factory extends PHPUnit\Runner\Filter\Factory {
+}
+
+class PHPUnit_Runner_Filter_IncludeGroupFilterIterator extends PHPUnit\Runner\Filter\IncludeGroupFilterIterator {
+}
+
+class PHPUnit_Runner_Filter_NameFilterIterator extends PHPUnit\Runner\Filter\NameFilterIterator {
+}
+
+class PHPUnit_Runner_PhptTestCase extends PHPUnit\Runner\PhptTestCase {
+}
+
+class PHPUnit_Runner_StandardTestSuiteLoader extends PHPUnit\Runner\StandardTestSuiteLoader {
+}
+
+class PHPUnit_Runner_Version extends PHPUnit\Runner\Version {
+}
+
+class PHPUnit_TextUI_Command extends PHPUnit\TextUI\Command {
+}
+
+class PHPUnit_Util_Printer extends PHPUnit\Util\Printer {
+}
+
+class PHPUnit_TextUI_ResultPrinter extends PHPUnit\TextUI\ResultPrinter {
+}
+
+class PHPUnit_TextUI_TestRunner extends PHPUnit\TextUI\TestRunner {
+}
+
+class PHPUnit_Util_Blacklist extends PHPUnit\Util\Blacklist {
+}
+
+class PHPUnit_Util_Configuration extends PHPUnit\Util\Configuration {
+}
+
+class PHPUnit_Util_ConfigurationGenerator extends PHPUnit\Util\ConfigurationGenerator {
+}
+
+class PHPUnit_Util_ErrorHandler extends PHPUnit\Util\ErrorHandler {
+}
+
+class PHPUnit_Util_Fileloader extends PHPUnit\Util\Fileloader {
+}
+
+class PHPUnit_Util_Filesystem extends PHPUnit\Util\Filesystem {
+}
+
+class PHPUnit_Util_Filter extends PHPUnit\Util\Filter {
+}
+
+class PHPUnit_Util_Getopt extends PHPUnit\Util\Getopt {
+}
+
+class PHPUnit_Util_GlobalState extends PHPUnit\Util\GlobalState {
+}
+
+class PHPUnit_Util_InvalidArgumentHelper extends PHPUnit\Util\InvalidArgumentHelper {
+}
+
+class PHPUnit_Util_Log_JUnit extends PHPUnit\Util\Log\JUnit {
+}
+
+class PHPUnit_Util_Log_TeamCity extends PHPUnit\Util\Log\TeamCity {
+}
+
+class PHPUnit_Util_PHP_AbstractPhpProcess extends PHPUnit\Util\PHP\AbstractPhpProcess {
+}
+
+class PHPUnit_Util_PHP_DefaultPhpProcess extends PHPUnit\Util\PHP\DefaultPhpProcess {
+}
+
+class PHPUnit_Util_PHP_WindowsPhpProcess extends PHPUnit\Util\PHP\WindowsPhpProcess {
+}
+
+class PHPUnit_Util_RegularExpression extends PHPUnit\Util\RegularExpression {
+}
+
+class PHPUnit_Util_Test extends PHPUnit\Util\Test {
+}
+
+class PHPUnit_Util_TestDox_ResultPrinter extends PHPUnit\Util\TestDox\ResultPrinter {
+}
+
+class PHPUnit_Util_TestDox_HtmlResultPrinter extends PHPUnit\Util\TestDox\HtmlResultPrinter {
+}
+
+class PHPUnit_Util_TestDox_NamePrettifier extends PHPUnit\Util\TestDox\NamePrettifier {
+}
+
+class PHPUnit_Util_TestDox_TextResultPrinter extends PHPUnit\Util\TestDox\TextResultPrinter {
+}
+
+class PHPUnit_Util_TestDox_XmlResultPrinter extends PHPUnit\Util\TestDox\XmlResultPrinter {
+}
+
+class PHPUnit_Util_Type extends PHPUnit\Util\Type {
+}
+
+class PHPUnit_Util_Xml extends PHPUnit\Util\Xml {
+}
+
+
+Fatal error: Uncaught PHPUnit\Runner\Exception: Class 'tests/phpunit-compat' could not be found in '/Users/crodas/projects/Prosspress/action-scheduler/tests/phpunit-compat.php'. in phar:///Users/crodas/Downloads/phpunit-6.1.3.phar/phpunit/Runner/StandardTestSuiteLoader.php on line 101
+
+PHPUnit\Runner\Exception: Class 'tests/phpunit-compat' could not be found in '/Users/crodas/projects/Prosspress/action-scheduler/tests/phpunit-compat.php'. in phar:///Users/crodas/Downloads/phpunit-6.1.3.phar/phpunit/Runner/StandardTestSuiteLoader.php on line 101
+
+Call Stack:
+    0.0148     803680   1. {main}() /Users/crodas/Downloads/phpunit-6.1.3.phar:0
+    0.0843    8012440   2. PHPUnit\TextUI\Command::main() /Users/crodas/Downloads/phpunit-6.1.3.phar:545
+    0.0843    8012552   3. PHPUnit\TextUI\Command->run() phar:///Users/crodas/Downloads/phpunit-6.1.3.phar/phpunit/TextUI/Command.php:140
+    0.0844    8014848   4. PHPUnit\Runner\BaseTestRunner->getTest() phar:///Users/crodas/Downloads/phpunit-6.1.3.phar/phpunit/TextUI/Command.php:161
+    0.0844    8014848   5. PHPUnit\Runner\BaseTestRunner->loadSuiteClass() phar:///Users/crodas/Downloads/phpunit-6.1.3.phar/phpunit/Runner/BaseTestRunner.php:71
+    0.0844    8014888   6. PHPUnit\Runner\StandardTestSuiteLoader->load() phar:///Users/crodas/Downloads/phpunit-6.1.3.phar/phpunit/Runner/BaseTestRunner.php:130
+

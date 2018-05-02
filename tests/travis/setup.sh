6a4d71b9849a1e96898bc2d7ddb92d26af7e7d0b
Jeremy Pry <jeremy.pry@gmail.com>
2018-05-02 10:08:48 -0400

    Update format of variables in setup.sh file

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -5,6 +5,8 @@
 # Author: Benjamin J. Balter ( ben@balter.com | ben.balter.com )
 # License: GPL3
 
+set -ev
+
 export WP_DIR=/tmp/wordpress
 export WP_CORE_DIR=${WP_DIR}/src
 export WP_TESTS_DIR=${WP_DIR}/tests/phpunit
@@ -17,7 +19,7 @@ then
 fi
 
 plugin_slug=$(basename $(pwd))
-plugin_dir=$WP_CORE_DIR/wp-content/plugins/$plugin_slug
+plugin_dir=${WP_CORE_DIR}/wp-content/plugins/${plugin_slug}
 
 # Init database
 mysql -e 'CREATE DATABASE wordpress_test;' -uroot
@@ -26,9 +28,9 @@ mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 git clone --quiet --depth=1 --branch="${WP_VERSION}" git://develop.git.wordpress.org/ ${WP_DIR}
 
 # Put various components in proper folders
-cp tests/travis/wp-tests-config.php $WP_TESTS_DIR/wp-tests-config.php
+cp tests/travis/wp-tests-config.php ${WP_TESTS_DIR}/wp-tests-config.php
 
 cd ..
-mv $plugin_slug $plugin_dir
+mv ${plugin_slug} ${plugin_dir}
 
-cd $plugin_dir
+cd ${plugin_dir}

95e66f266d26f0f9a09ed9f37b0a17a83cca5568
Jeremy Pry <jeremy.pry@gmail.com>
2018-05-04 08:56:27 -0400

    Move travis/setup.sh into dedicated setup directory
    Also:
    - Use uppercase for variables. This is the normal convention in shell scripts
    - Use quotes around variables
    - Ensure we have the WP_VERSION variable available. This is just extra insurance. It probably will never be undefined
    - Update author info
    - Only create DB if it doesn't exist

---
+++ tests/setup/travis.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# WordPress test setup script for Travis CI
+#
+# Author: Benjamin J. Balter ( ben@balter.com | ben.balter.com ), Prospress, Jeremy Pry
+# License: GPL3
+
+set -ev
+
+# Set necessary variables.
+export WP_DIR=/tmp/wordpress
+export WP_CORE_DIR="${WP_DIR}/src"
+export WP_TESTS_DIR="${WP_DIR}/tests/phpunit"
+
+# Ensure we have a WP_VERSION
+WP_VERSION="${WP_VERSION:-master}"
+
+if [[ "$1" = "5.6" || "$1" > "5.6" ]]
+then
+  wget -c https://phar.phpunit.de/phpunit-5.7.phar
+  chmod +x phpunit-5.7.phar
+  mv phpunit-5.7.phar `which phpunit`
+fi
+
+PLUGIN_SLUG=$(basename $(pwd))
+PLUGIN_DIR=${WP_CORE_DIR}/wp-content/plugins/${PLUGIN_SLUG}
+
+# Init database
+mysql -e 'CREATE DATABASE IF NOT EXISTS wordpress_test;' -uroot
+
+# Grab specified version of WordPress
+git clone --quiet --depth=1 --branch="${WP_VERSION}" git://develop.git.wordpress.org/ "${WP_DIR}"
+
+# Put various components in proper folders
+cp tests/travis/wp-tests-config.php ${WP_TESTS_DIR}/wp-tests-config.php
+
+cd ..
+mv ${PLUGIN_SLUG} ${PLUGIN_DIR}
+
+cd ${PLUGIN_DIR}

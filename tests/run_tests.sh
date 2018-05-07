097e2d20cf6e30211f1ab1de595bf99047ac9c39
Jeremy Pry <jeremy.pry@gmail.com>
2018-05-07 14:17:36 -0400

    Update script for running tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ "${TRAVIS_PHP_VERSION}" > "5.6" || "${TRAVIS_PHP_VERSION}" = "5.6" ]]
+set -ev
+
+# Use composer phpunit for newer PHP versions, or if we're not in Travis.
+if [[ "${TRAVIS_PHP_VERSION}" > "5.6" || "${TRAVIS_PHP_VERSION}" = "5.6" || -z "${TRAVIS_PHP_VERSION}" ]]
 then
 	PHPUNIT="vendor/bin/phpunit"
 else
 	PHPUNIT="phpunit"
 fi
 
-if [[ "${TRAVIS_PHP_VERSION}" == "7.1" ]] && [[ "${WP_VERSION}" == "${WP_LATEST}" ]] && [[ "${WP_MULTISITE}" == "0" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]
+# Maybe run code coverage report.
+if [[ "${TRAVIS_PHP_VERSION}" = "7.1" && "${WP_VERSION}" = "${WP_LATEST}" && "${WP_MULTISITE}" = "0" && "${TRAVIS_BRANCH}" = "master" ]]
 then
 	export AS_CODE_COVERAGE=1
 	"${PHPUNIT}" --configuration tests/phpunit.xml.dist --coverage-clover clover.xml

95a5f81206e5308767cea81dc9a296b9d7b5ec48
Jeremy Pry <jeremy.pry@gmail.com>
2018-05-07 10:29:04 -0400

    Run appropriate phpunit depending on PHP version
    Also:
    - Move composer install to travis script
    - Only run code coverage when needed, and only after a successful run
    - Cache some composer files
    - Update script for running tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_PHP_VERSION" == "7.1" ]] && [[ "$WP_VERSION" == "4.8" ]] && [[ "$WP_MULTISITE" == "0" ]] && [[ "$TRAVIS_BRANCH" == "master" ]]
+set -ev
+
+# Use composer phpunit for newer PHP versions, or if we're not in Travis.
+if [[ "${TRAVIS_PHP_VERSION}" > "5.6" || "${TRAVIS_PHP_VERSION}" = "5.6" || -z "${TRAVIS_PHP_VERSION}" ]]
 then
-	vendor/bin/phpunit --configuration tests/phpunit.xml.dist --coverage-clover clover.xml
+	PHPUNIT="vendor/bin/phpunit"
 else
-	vendor/bin/phpunit --configuration tests/phpunit.xml.dist
+	PHPUNIT="phpunit"
+fi
+
+# Maybe run code coverage report.
+if [[ "${TRAVIS_PHP_VERSION}" = "7.1" && "${WP_VERSION}" = "${WP_LATEST}" && "${WP_MULTISITE}" = "0" && "${TRAVIS_BRANCH}" = "master" ]]
+then
+	export AS_CODE_COVERAGE=1
+	"${PHPUNIT}" --configuration tests/phpunit.xml.dist --coverage-clover clover.xml
+else
+	"${PHPUNIT}" --configuration tests/phpunit.xml.dist
 fi

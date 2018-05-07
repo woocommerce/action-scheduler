023a72cd0968bffc235a650b01c462114834c5c2
Jeremy Pry <jeremy.pry@gmail.com>
2018-05-07 10:44:15 -0400

    Only run code coverage when needed

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,6 +9,7 @@ fi
 
 if [[ "${TRAVIS_PHP_VERSION}" == "7.1" ]] && [[ "${WP_VERSION}" == "${WP_LATEST}" ]] && [[ "${WP_MULTISITE}" == "0" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]
 then
+	export AS_CODE_COVERAGE=1
 	"${PHPUNIT}" --configuration tests/phpunit.xml.dist --coverage-clover clover.xml
 else
 	"${PHPUNIT}" --configuration tests/phpunit.xml.dist

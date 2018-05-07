b7b465e1dfb564942fa768dd71ad3b3df6b4b137
Jeremy Pry <jeremy.pry@gmail.com>
2018-05-07 10:29:04 -0400

    Run appropriate phpunit depending on PHP version

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_PHP_VERSION" == "7.1" ]] && [[ "$WP_VERSION" == "4.8" ]] && [[ "$WP_MULTISITE" == "0" ]] && [[ "$TRAVIS_BRANCH" == "master" ]]
+if [[ "${TRAVIS_PHP_VERSION}" -ge "5.6" ]]
 then
-	vendor/bin/phpunit --configuration tests/phpunit.xml.dist --coverage-clover clover.xml
+	PHPUNIT="vendor/bin/phpunit"
 else
-	vendor/bin/phpunit --configuration tests/phpunit.xml.dist
+	PHPUNIT="phpunit"
+fi
+
+if [[ "${TRAVIS_PHP_VERSION}" == "7.1" ]] && [[ "${WP_VERSION}" == "${WP_LATEST}" ]] && [[ "${WP_MULTISITE}" == "0" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]
+then
+	"${PHPUNIT}" --configuration tests/phpunit.xml.dist --coverage-clover clover.xml
+else
+	"${PHPUNIT}" --configuration tests/phpunit.xml.dist
 fi

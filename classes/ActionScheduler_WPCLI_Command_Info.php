6282619ce645237b95922453f75964a1529b2ac5
Caleb Stauffer <develop+github@calebstauffer.com>
2019-04-08 00:32:56 -0400

    simplify command

--- classes/ActionScheduler_WPCLI_Command_Info.php
+++ classes/ActionScheduler_WPCLI_Command_Info.php
@@ -6,167 +6,24 @@ class ActionScheduler_WPCLI_Command_Info extends ActionScheduler_Abstract_WPCLI_
 	 * Execute command.
 	 */
 	public function execute() {
-		$hooks = \WP_CLI\Utils\get_flag_value( $this->assoc_args, 'hooks', false );
-		$past_due = \WP_CLI\Utils\get_flag_value( $this->assoc_args, 'past-due', false );
-
-		if ( $past_due ) {
-			$this->past_due( $hooks );
-		} else if ( $hooks ) {
-			$this->hooks( $hooks );
-		} else {
-			$this->default();
-		}
+		$this->completed_actions();
+		$this->pending_actions();
+		$this->overdue_actions();
 	}
 
-	/**
-	 * Print info on past due actions.
-	 *
-	 * @param string $hooks
-	 */
-	protected function past_due( $hooks ) {
-		$store = ActionScheduler::store();
-		$columns = explode( ',', \WP_CLI\Utils\get_flag_value( $this->assoc_args, 'columns', 'hook,total' ) );
-
-		if ( true === $hooks ) {
-			$hooks = $store->action_hooks();
-		} else if ( !empty( $hooks ) ) {
-			$hooks = explode( ',', $hooks );
-		}
-
-		$args = array(
-			'status' => 'pending',
-			'date' => as_get_datetime_object()->format( 'Y-m-d H:i:s' ),
-		);
-
-		if ( !empty( $hooks ) ) {
-			$rows = array();
-
-			foreach ( $hooks as $hook ) {
-				$args['hook'] = $hook;
-				$count = ( int ) $store->query_actions( $args, 'count' );
-
-				if ( 0 === $count )
-					continue;
-
-				$row = array(
-					'hook' => $hook,
-					'total' => $count,
-				);
-
-				if ( count( array_intersect( array( 'oldest', 'newest' ), $columns ) ) ) {
-					foreach ( array( 'oldest', 'newest' ) as $point ) {
-						$actions = $store->query_actions( array(
-							'hook'     => $hook,
-							'status'   => 'pending',
-							'claimed'  => false,
-							'per_page' => 1,
-							'order'    => ( 'oldest' === $point ? 'ASC' : 'DESC' ),
-							'date'     => as_get_datetime_object()->format( 'Y-m-d H:i:s' ),
-						) );
-
-						if ( ! empty( $actions ) ) {
-							$date_object = $store->get_date( $actions[0] );
-							$row[ $point ] = $date_object->format( 'Y-m-d H:i:s O' );
-						}
-					}
-				}
-
-				$rows[] = $row;
-			}
-
-			$this->table( $rows, $columns );
-		} else {
-			unset( $args['hook'] );
-			$this->log( 'Total past due: ' . $store->query_actions( $args, 'count' ) );
-
-			$args['order'] = 'ASC';
-			$actions = $store->query_actions( $args );
-			$date_object = $store->get_date( $actions[0] );
-			$this->log( 'Oldest: ' . $date_object->format( 'Y-m-d H:i:s O' ) );
-		}
+	protected function completed_actions() {
+		$completed_actions = (int) $this->store->query_actions( array( 'status' => $this->store::STATUS_COMPLETE ), 'count' );
+		$this->log( 'Completed actions: ' . $completed_actions );
 	}
 
-	/**
-	 * Print counts for hooks.
-	 *
-	 * @param string $hooks
-	 */
-	protected function hooks( $hooks ) {
-		$store = ActionScheduler::store();
-		$stati = array_keys( $store->get_status_labels() );
-		$columns = explode( ',', \WP_CLI\Utils\get_flag_value( $this->assoc_args, 'columns', 'hook,total' ) );
-
-		$rows = array();
-
-		if ( true === $hooks ) {
-			$hooks = $store->action_hooks();
-		} else {
-			$hooks = explode( ',', $hooks );
-		}
-
-		foreach ( $hooks as $hook ) {
-			$row = $args = array( 'hook' => $hook );
-
-			if ( in_array( 'total', $columns ) ) {
-				$row['total'] = ( int ) $store->query_actions( array( 'hook' => $hook ), 'count' );
-			}
-
-			if ( count( array_intersect( $stati, $columns ) ) ) {
-				foreach ( array_intersect( $columns, $stati ) as $status ) {
-					$args['status'] = $status;
-					$count = ( int ) $store->query_actions( $args, 'count' );
-					$row[ $status ] = $count;
-				}
-			}
-
-			if ( count( array_intersect( array( 'oldest', 'newest' ), $columns ) ) ) {
-				foreach ( array( 'oldest', 'newest' ) as $point ) {
-					$action = $store->query_actions( array(
-						'hook' => $hook,
-						'claimed'  => false,
-						'per_page' => 1,
-						'order'    => ( 'oldest' === $point ? 'ASC' : 'DESC' ),
-					) );
-
-					if ( ! empty( $action ) ) {
-						$date_object = $store->get_date( $action[0] );
-						$row[ $point ] = $date_object->format( 'Y-m-d H:i:s O' );
-					}
-				}
-			}
-
-			$rows[] = $row;
-		}
-
-		$this->table( $rows, $columns );
+	protected function pending_actions() {
+		$pending_actions = (int) $this->store->query_actions( array( 'status' => $this->store::STATUS_PENDING ), 'count' );
+		$this->log( 'Pending actions: ' . $pending_actions );
 	}
 
-	/**
-	 * Print counts for all statuses.
-	 */
-	protected function default() {
-		$store = ActionScheduler::store();
-		$columns = explode( ',', \WP_CLI\Utils\get_flag_value( $this->assoc_args, 'columns', 'status,count' ) );
-		$status_labels = $store->get_status_labels();
-		$action_counts = $store->action_counts();
-		$status_dates = $store->action_dates();
-
-		$rows = array();
-		$total = 0;
-
-		foreach ( $status_labels as $post_status => $label ) {
-			$rows[] = array(
-				'status' => $label,
-				'count' => $action_counts[ $post_status ],
-				'oldest' => $status_dates[ $post_status ]['oldest'],
-				'newest' => $status_dates[ $post_status ]['newest'],
-			);
-
-			$total += $action_counts[ $post_status ];
-		}
-
-		$this->table( $rows, $columns );
-		$this->log( 'Total: ' . $total );
+	protected function overdue_actions() {
+		$overdue_actions = (int) $this->store->query_actions( array( 'status' => $this->store::STATUS_PENDING, 'date' => as_get_datetime_object()->format( 'Y-m-d H:i:s' ) ), 'count' );
+		$this->log( 'Overdue actions: ' . $overdue_actions );
 	}
 
 }

d896f81f87ff486287735a1f01294f60e3bfaac8
Caleb Stauffer <develop+github@calebstauffer.com>
2019-04-09 00:17:52 -0400

    init `action` command

---
+++ classes/ActionScheduler_WPCLI_Command_Action.php
@@ -0,0 +1,68 @@
+<?php
+
+class ActionScheduler_WPCLI_Command_Action {
+
+	/**
+	 * Creates an action.
+	 */
+	public function create() {
+
+	}
+
+	/**
+	 * Deletes an existing action.
+	 */
+	public function delete() {
+
+	}
+
+	/**
+	 * Duplicates an existing action.
+	 */
+	public function duplicate() {
+
+	}
+
+	/**
+	 * Vertifies whether an action exists.
+	 */
+	public function exists() {
+
+	}
+
+	/**
+	 * Generates some actions.
+	 */
+	public function generate() {
+
+	}
+
+	/**
+	 * Get details about an action.
+	 */
+	public function get() {
+
+	}
+
+	/**
+	 * Gets a list of actions.
+	 */
+	public function list() {
+
+	}
+
+	/**
+	 * Runs the action.
+	 */
+	public function run() {
+
+	}
+
+	/**
+	 * Updates one or more existing actions.
+	 */
+	public function update() {
+
+	}
+
+}

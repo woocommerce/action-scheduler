790711bab5b92205377c232b863c0060d33afbbc
Ron Rennick <ron@ronandandrea.com>
2020-02-18 11:27:07 -0400

    always hook initialize_latest_version()

--- action-scheduler.php
+++ action-scheduler.php
@@ -29,9 +29,9 @@ if ( ! function_exists( 'action_scheduler_register_3_dot_1_dot_0' ) ) {
 
 	if ( ! class_exists( 'ActionScheduler_Versions' ) ) {
 		require_once( 'classes/ActionScheduler_Versions.php' );
-		add_action( 'plugins_loaded', array( 'ActionScheduler_Versions', 'initialize_latest_version' ), 1, 0 );
 	}
 
+	add_action( 'plugins_loaded', array( 'ActionScheduler_Versions', 'initialize_latest_version' ), 1, 0 );
 	add_action( 'plugins_loaded', 'action_scheduler_register_3_dot_1_dot_0', 0, 0 );
 
 	function action_scheduler_register_3_dot_1_dot_0() {

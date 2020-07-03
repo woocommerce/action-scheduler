33d73da47ab22610925b2e623d8daba9bbe44e10
Alexandre Faustino <alexmigf@gmail.com>
2020-07-03 12:42:10 +0100

    Update classes/data-stores/ActionScheduler_DBStore.php
    Co-authored-by: Abhishek Pokhriyal <pokhi@coloredcow.com>

--- classes/data-stores/ActionScheduler_DBStore.php
+++ classes/data-stores/ActionScheduler_DBStore.php
@@ -309,7 +309,7 @@ class ActionScheduler_DBStore extends ActionScheduler_Store {
 		}
 
 		if ( ! is_null( $query[ 'args' ] ) ) {
-			if( isset($query[ 'args_partial_match' ]) && $query[ 'args_partial_match' ] === true && $mysql_version >= '5.7' ) {
+			if ( isset( $query['args_partial_match'] ) && true === $query['args_partial_match'] && $mysql_version >= '5.7' ) {
 				foreach ( $query[ 'args' ] as $key => $value ) {
 					$supported_types = [ 'integer', 'boolean', 'double', 'string' ];
 					if ( !in_array( gettype( $value ), $supported_types ) ) {

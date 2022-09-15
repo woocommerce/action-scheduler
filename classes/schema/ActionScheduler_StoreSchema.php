f5a7521c1c6d252ad994a84a62052ae7a14a5020
Saad Siddique <saad@uncannyowl.com>
2022-09-15 13:33:55 -0400

    Error saving action: `ActionScheduler_Action::$args` too long
    Closes #851
    
    PHP Fatal error: Uncaught RuntimeException: Error saving action: ActionScheduler_Action::$args too long

--- classes/schema/ActionScheduler_StoreSchema.php
+++ classes/schema/ActionScheduler_StoreSchema.php
@@ -56,7 +56,7 @@ class ActionScheduler_StoreSchema extends ActionScheduler_Abstract_Schema {
 				        last_attempt_gmt datetime NULL default '${default_date}',
 				        last_attempt_local datetime NULL default '${default_date}',
 				        claim_id bigint(20) unsigned NOT NULL default '0',
-				        extended_args varchar(8000) DEFAULT NULL,
+				        extended_args longtext,
 				        PRIMARY KEY  (action_id),
 				        KEY hook (hook($max_index_length)),
 				        KEY status (status),

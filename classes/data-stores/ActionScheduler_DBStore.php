f217845cae4dcb20dd2d38285d0765680adb8b93
Alexandre Faustino <alexmigf@gmail.com>
2022-10-11 15:26:45 +0100

    Small tweak

--- classes/data-stores/ActionScheduler_DBStore.php
+++ classes/data-stores/ActionScheduler_DBStore.php
@@ -415,7 +415,7 @@ AND `group_id` = %d
 			$sql_params[] = $query['hook'];
 		}
 
-		if ( ! is_null( $query['args'] ) && ! empty( $query['partial_args_matching'] ) ) {
+		if ( ! is_null( $query['args'] ) ) {
 			switch ( $query['partial_args_matching'] ) {
 				case 'json':
 					if ( $mysql_version < '5.7' ) {

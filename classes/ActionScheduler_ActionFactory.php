63330f0fe583f0dab0b4828ca66718d4830273be
Jonathan Stegall <jonathan@jonathanstegall.com>
2022-11-03 08:06:27 -0500

    correclty order the parameters for the calls to `single_unique`

--- classes/ActionScheduler_ActionFactory.php
+++ classes/ActionScheduler_ActionFactory.php
@@ -146,7 +146,7 @@ class ActionScheduler_ActionFactory {
 	 */
 	public function recurring_unique( $hook, $args = array(), $first = null, $interval = null, $group = '', $unique = true ) {
 		if ( empty( $interval ) ) {
-			return $this->single_unique( $hook, $unique, $args, $first, $group );
+			return $this->single_unique( $hook, $args, $first, $group, $unique );
 		}
 		$date     = as_get_datetime_object( $first );
 		$schedule = new ActionScheduler_IntervalSchedule( $date, $interval );
@@ -188,7 +188,7 @@ class ActionScheduler_ActionFactory {
 	 **/
 	public function cron_unique( $hook, $args = array(), $base_timestamp = null, $schedule = null, $group = '', $unique = true ) {
 		if ( empty( $schedule ) ) {
-			return $this->single_unique( $hook, $unique, $args, $base_timestamp, $group );
+			return $this->single_unique( $hook, $args, $base_timestamp, $group, $unique );
 		}
 		$date     = as_get_datetime_object( $base_timestamp );
 		$cron     = CronExpression::factory( $schedule );

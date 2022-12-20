11e5f90e32ebd599c222ce150ed0db463058b8c2
Stephy Miehle <stephy.miehle@gouconnect.com>
2022-12-19 18:04:37 -0600

    Add early return opportunity; minor linting updates

--- classes/ActionScheduler_AdminView.php
+++ classes/ActionScheduler_AdminView.php
@@ -147,13 +147,16 @@ class ActionScheduler_AdminView extends ActionScheduler_AdminView_Deprecated {
 		$threshold_seconds = ( int ) apply_filters( 'action_scheduler_pastdue_actions_seconds', DAY_IN_SECONDS );
 		$threshhold_min    = ( int ) apply_filters( 'action_scheduler_pastdue_actions_min', 1 );
 
-		# Set fallback value for past-due actions count.
-		if ( ! isset( $num_pastdue_actions ) ) {
-			$num_pastdue_actions = 0;
-		}
+		// Set fallback value for past-due actions count.
+		$num_pastdue_actions = 0;
 
-		# Allow third-parties to preempt the default check logic.
+		// Allow third-parties to preempt the default check logic.
 		$check = apply_filters( 'action_scheduler_pastdue_actions_check_pre', null );
+
+		// If no third-party preempted and there are no past-due actions, return early.
+		if ( ! is_null( $check ) ) {
+			return;
+		}
 
 		# Scheduled actions query arguments.
 		$query_args = array(

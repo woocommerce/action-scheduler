70b028e0117fd4db5ac4b352345db05877157ef7
Caleb Stauffer <develop+github@calebstauffer.com>
2024-08-05 19:24:33 -0400

    save

--- classes/ActionScheduler_PastDueMonitor.php
+++ classes/ActionScheduler_PastDueMonitor.php
@@ -7,6 +7,9 @@
  */
 class ActionScheduler_PastDueMonitor {
 
+	const TRANSIENT_LAST_EMAIL = 'action_scheduler_pastdue_actions_last_email';
+	const TRANSIENT_CHECK_INTERVAL = 'action_scheduler_last_pastdue_actions_check';
+
 	private static $monitor = null;
 
 	protected $threshold_seconds;
@@ -32,6 +35,11 @@ class ActionScheduler_PastDueMonitor {
 		return self::$monitor;
 	}
 
+	/**
+	 * Initialize.
+	 *
+	 * @return void
+	 */
 	public function init() {
 		$this->threshold_seconds       = absint( apply_filters( 'action_scheduler_pastdue_actions_seconds', DAY_IN_SECONDS ) );
 		$this->threshold_minimum       = absint( apply_filters( 'action_scheduler_pastdue_actions_min', 1 ) );
@@ -39,19 +47,72 @@ class ActionScheduler_PastDueMonitor {
 		$this->interval_email_seconds  = absint( apply_filters( 'action_scheduler_pastdue_actions_email_interval', HOUR_IN_SECONDS ) );
 		$this->threshold_email_minimum = absint( apply_filters( 'action_scheduler_pastdue_actions_email_min', $this->threshold_minimum ) );
 
-		$this->maybe_send_email();
+		add_action( 'action_scheduler_stored_action', array( $this, 'maybe_send_email' ) );
 
 		if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || false == DOING_AJAX ) ) {
 			add_action( 'admin_notices', array( $this, 'action__admin_notices' ) );
 		}
 	}
 
-	protected function maybe_send_email() {
-		if ( ! $this->critical() ) {
+	/**
+	 * Check if threshold for past-due actions is flooded.
+	 *
+	 * @return bool
+	 */
+	protected function flooded() {
+		$transient = get_transient( self::TRANSIENT_CHECK_INTERVAL );
+
+		if ( ! empty( $transient ) ) {
+			return false;
+		}
+
+		# Scheduled actions query arguments.
+		$this->query_args = array(
+			'date'     => as_get_datetime_object( time() - $this->threshold_seconds ),
+			'status'   => ActionScheduler_Store::STATUS_PENDING,
+			'per_page' => $this->threshold_minimum,
+		);
+
+		$store = ActionScheduler_Store::instance();
+		$this->num_pastdue_actions = ( int ) $store->query_actions( $this->query_args, 'count' );
+
+		# Check if past-due actions count is greater than or equal to threshold.
+		$flooded = ( $this->num_pastdue_actions >= $this->threshold_minimum );
+		$flooded = ( bool ) apply_filters( 'action_scheduler_pastdue_actions_check', $flooded, $this->num_pastdue_actions, $this->threshold_seconds, $this->threshold_minimum );
+
+		# If not flooded, delay next check.
+		if ( ! $flooded ) {
+			set_transient( self::TRANSIENT_CHECK_INTERVAL, time(), $this->interval_check );
+		}
+
+		return $flooded;
+	}
+
+	/**
+	 * Action: action_scheduler_stored_action
+	 *
+	 * Maybe send email notice of past-due actions over threshold.
+	 *
+	 * @return void
+	 */
+	public function maybe_send_email() {
+		if ( 'action_scheduler_stored_action' !== current_action() ) {
 			return;
 		}
 
-		$transient = get_transient( 'action_scheduler_pastdue_actions_last_email' );
+		// Allow third-parties to preempt the default check logic.
+		$pre = apply_filters( 'action_scheduler_pastdue_actions_check_pre', null );
+
+		// If no third-party preempted and there are no past-due actions, return early.
+		if ( ! is_null( $pre ) ) {
+			return;
+		}
+
+		if ( ! $this->flooded() ) {
+			return;
+		}
+
+		$transient = get_transient( self::TRANSIENT_LAST_EMAIL );
 
 		if ( ! empty( $transient ) ) {
 			return;
@@ -73,49 +134,11 @@ class ActionScheduler_PastDueMonitor {
 		$subject = sprintf( 'Action Scheduler: past-due scheduled actions (%s)', $sitename );
 		$message = $this->message();
 
-		set_transient( 'action_scheduler_pastdue_actions_last_email', time(), $this->interval_email_seconds );
+		set_transient( self::TRANSIENT_LAST_EMAIL, time(), $this->interval_email_seconds );
 
-		wp_mail( $to, $subject, $message );
-	}
-
-	protected function critical() {
-		// Allow third-parties to preempt the default check logic.
-		$check = apply_filters( 'action_scheduler_pastdue_actions_check_pre', null );
-
-		// If no third-party preempted and there are no past-due actions, return early.
-		if ( ! is_null( $check ) ) {
-			return $check;
-		}
-
-		if ( ! empty( $this->num_pastdue_actions ) ) {
-			return true;
-		}
-
-		$transient = get_transient( 'action_scheduler_pastdue_actions_pause' );
-
-		if ( ! empty( $transient ) ) {
-			return false;
-		}
-
-		# Scheduled actions query arguments.
-		$this->query_args = array(
-			'date'     => as_get_datetime_object( time() - $this->threshold_seconds ),
-			'status'   => ActionScheduler_Store::STATUS_PENDING,
-			'per_page' => $this->threshold_minimum,
-		);
-
-		$store = ActionScheduler_Store::instance();
-		$this->num_pastdue_actions = ( int ) $store->query_actions( $this->query_args, 'count' );
-
-		# Check if past-due actions count is greater than or equal to threshold.
-		$check = ( $this->num_pastdue_actions >= $this->threshold_minimum );
-		$check = ( bool ) apply_filters( 'action_scheduler_pastdue_actions_check', $check, $this->num_pastdue_actions, $this->threshold_seconds, $this->threshold_minimum );
-
-		if ( ! $check ) {
-			set_transient( 'action_scheduler_pastdue_actions_pause', time(), $this->interval_check );
-		}
-
-		return $check;
+		wp_mail( $to, $subject, $message, array(
+			'Content-type: text/html; charset=UTF-8',
+		) );
 	}
 
 	/**
@@ -133,7 +156,15 @@ class ActionScheduler_PastDueMonitor {
 			return;
 		}
 
-		if ( ! $this->critical() ) {
+		// Allow third-parties to preempt the default check logic.
+		$pre = apply_filters( 'action_scheduler_pastdue_actions_check_pre', null );
+
+		// If no third-party preempted and there are no past-due actions, return early.
+		if ( ! is_null( $pre ) ) {
+			return;
+		}
+
+		if ( ! $this->flooded() ) {
 			return;
 		}
 
@@ -146,6 +177,11 @@ class ActionScheduler_PastDueMonitor {
 		do_action( 'action_scheduler_pastdue_actions_extra_notices', $this->query_args );
 	}
 
+	/**
+	 * Message for admin notice and email notice.
+	 *
+	 * @return string
+	 */
 	protected function message() {
 		$actions_url = add_query_arg( array(
 			'page'   => 'action-scheduler',

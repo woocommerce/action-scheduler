f43d43a92dc700cfe94c2f1d5cce2d86e88d4016
Caleb Stauffer <develop+github@calebstauffer.com>
2024-10-03 20:21:25 -0400

    eliminate code smells

--- classes/abstracts/ActionScheduler_Abstract_Schedule.php
+++ classes/abstracts/ActionScheduler_Abstract_Schedule.php
@@ -10,16 +10,18 @@ abstract class ActionScheduler_Abstract_Schedule extends ActionScheduler_Schedul
 	 *
 	 * @var DateTime
 	 */
-	private $scheduled_date = NULL;
+	private $scheduled_date = null;
 
 	/**
 	 * Timestamp equivalent of @see $this->scheduled_date
 	 *
 	 * @var int
 	 */
-	protected $scheduled_timestamp = NULL;
+	protected $scheduled_timestamp = null;
 
 	/**
+	 * Construct.
+	 *
 	 * @param DateTime $date The date & time to run the action.
 	 */
 	public function __construct( DateTime $date ) {
@@ -66,7 +68,8 @@ abstract class ActionScheduler_Abstract_Schedule extends ActionScheduler_Schedul
 	}
 
 	/**
-	 * For PHP 5.2 compat, since DateTime objects can't be serialized
+	 * For PHP 5.2 compat, because DateTime objects can't be serialized
+	 *
 	 * @return array
 	 */
 	public function __sleep() {

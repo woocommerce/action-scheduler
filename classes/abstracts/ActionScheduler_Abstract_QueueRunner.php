050d05dee5aeed5f9fda8d4187ac580a04e4d655
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2025-03-27 14:58:19 +0100

    Fix custom error handler not restored if fatal
    Fix https://github.com/woocommerce/action-scheduler/issues/1253

--- classes/abstracts/ActionScheduler_Abstract_QueueRunner.php
+++ classes/abstracts/ActionScheduler_Abstract_QueueRunner.php
@@ -78,6 +78,7 @@ abstract class ActionScheduler_Abstract_QueueRunner extends ActionScheduler_Abst
 			},
 			E_USER_ERROR | E_RECOVERABLE_ERROR
 		);
+		add_action( 'shutdown', 'restore_error_handler', -10 );
 
 		/*
 		 * The nested try/catch structure is required because we potentially need to convert thrown errors into
@@ -112,6 +113,7 @@ abstract class ActionScheduler_Abstract_QueueRunner extends ActionScheduler_Abst
 			$this->handle_action_error( $action_id, $e, $context, $valid_action );
 		} finally {
 			restore_error_handler();
+			remove_action( 'shutdown', 'restore_error_handler', -10 );
 		}
 
 		if ( isset( $action ) && is_a( $action, 'ActionScheduler_Action' ) && $action->get_schedule()->is_recurring() ) {

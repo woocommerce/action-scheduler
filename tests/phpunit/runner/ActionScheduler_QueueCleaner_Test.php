615b3ed828f81a641b502125532df129f360bd3c
kalessil <kalessil@gmail.com>
2026-04-02 10:42:14 +0200

    Address code review feedback.

--- tests/phpunit/runner/ActionScheduler_QueueCleaner_Test.php
+++ tests/phpunit/runner/ActionScheduler_QueueCleaner_Test.php
@@ -391,6 +391,7 @@ class ActionScheduler_QueueCleaner_Test extends ActionScheduler_UnitTestCase {
 
 		remove_filter( 'action_scheduler_default_cleaner_statuses', $filter_statuses );
 		remove_filter( 'pre_as_schedule_single_action', $filter_as_schedule );
+		remove_action( 'action_scheduler_run_actions_cleanup_hook',  array( $cleaner, 'delete_old_actions' ) );
 	}
 
 	/**
@@ -432,5 +433,6 @@ class ActionScheduler_QueueCleaner_Test extends ActionScheduler_UnitTestCase {
 
 		remove_filter( 'action_scheduler_default_cleaner_statuses', $filter_statuses );
 		remove_filter( 'pre_as_schedule_single_action', $filter_as_schedule );
+		remove_action( 'action_scheduler_run_actions_cleanup_hook',  array( $cleaner, 'delete_old_actions' ) );
 	}
 }
